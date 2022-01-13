354be393e32424ae2107d4a8ef7e59d79f8885e5
Steven Sousa <70921007+Stevensousa67@users.noreply.github.com>
2022-01-12 19:50:24 -0500

    Updated MAC_Changer

--- Personal_Projects/Python & Ethical Hacking/MAC_Changer.py
+++ Personal_Projects/Python & Ethical Hacking/MAC_Changer.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python
 import subprocess
 import optparse
+import re
 
 
 def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option('-i', '--interface', dest='interface', help='Interface to change MAC address')
-    parser.add_option('-m', '--mac', dest='new_MAC', help='New MAC address')
+    parser.add_option('-m', '--mac', dest='new_mac', help='New MAC address')
     (options, arguments) = parser.parse_args()
     if not options.interface:
         parser.error("[-] Please specify an interface. Use --help for more info.")
@@ -15,11 +17,27 @@ def get_arguments():
 
 
 def change_mac(interface, new_mac):
-    print("[+] Changing MAC address for " + interface)
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(['ifconfig', interface, 'down'])
     subprocess.call(['ifconfig', interface, 'ether', 'hw', new_mac])
     subprocess.call(['ifconfig', interface, 'up'])
 
 
-option = get_arguments()
-change_mac(option.interface, option.new_mac)
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not get MAC address")
+
+
+options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("[+] Current MAC address: " + str(current_mac))
+change_mac(options.interface, options.new_mac)
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] New MAC address was successfully changed to " + current_mac)
+else:
+    print("[-] MAC address did not get changed.")
